b24b679d5e0f80f2fd5dca207604c57a28213260
Alumno <alumno@localhost.localdomain>
2014-01-29 08:27:18 -0600

    prueba

---
+++ Cfc1234.java
@@ -0,0 +1,16 @@
+package claudio123;
+
+import java.util.Scanner;
+
+public class Cfc1234 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner mi_scanner = new Scanner(System.in);
+int mi_variable = mi_scanner.nextInt();
+if (mi_variable == 10)
+{System.out.print("Es un 10");
+}
+	}
+
+}
